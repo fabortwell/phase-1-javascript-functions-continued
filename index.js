a345d5ac7db7d97af909b2b59f9e780ea4556028
Brian Sitati <briansit93@gmail.com>
2022-04-08 15:32:25 +0300

    passed some parts of the tests

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun () {
+ return "This Saturday, I want to roller-skate!";
+
+   }
+   function saturdayFun () {
+    return 'This Saturday, I want to bathe my dog!';
+   }
+saturdayFun ();
+
+function mondayWork () {
+    return 'This Monday, I will go to the office.';
+}
+function mondayWork () {
+   return 'This Monday, I will work from home.';
+}
+mondayWork ();
